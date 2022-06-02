12a02734a5b7ece568622effdf1d8ce88d79d19b
Maksimov <amaksimov744@gmail.com>
2022-06-03 00:31:25 +0300

    Добрый день. Выполнены все задания к 7ой лекции. Не получилось настроить отправку писем через внешний ресурс. Т.е. письма с настройками внешнего ресурса в папку падают, но на внешний почтовый ящик не уходят. EMAIL_BACKEND=django.core.mail.backends.smtp.EmailBackend указывал. Redis и RabbitMQ для Celery запускал в контейнерах Docker. В файлах файл только для Rabbit, Redis через терминал запускал без файла.

--- mainapp/views.py
+++ mainapp/views.py
@@ -1,13 +1,26 @@
-from django.contrib.auth.mixins import PermissionRequiredMixin, LoginRequiredMixin
-from django.http import JsonResponse
+from django.conf import settings
+from django.contrib.auth.mixins import PermissionRequiredMixin, LoginRequiredMixin, UserPassesTestMixin
+from django.http import JsonResponse, FileResponse
 from django.shortcuts import get_object_or_404
 from django.template.loader import render_to_string
-
+from django.core.cache import cache
 from django.urls import reverse_lazy
+from django.views import View
 from django.views.generic import TemplateView, ListView, DetailView, CreateView, UpdateView, DeleteView
+from django.utils.translation import gettext_lazy as _
+
+from mainapp import tasks
+from mainapp import tasks as mainapp_tasks
+from django.contrib import messages
+from django.http.response import HttpResponseRedirect
 
 from mainapp.forms import CourseFeedbackForm
 from mainapp.models import News, Courses, Lesson, CourseTeachers, CourseFeedback
+
+import logging
+
+
+logger = logging.getLogger(__name__)
 
 
 class ContactsView(TemplateView):
@@ -37,6 +50,12 @@ class ContactsView(TemplateView):
             }
         ]
         return context_data
+
+    def post(self, *args, **kwargs):
+        message_body = self.request.POST.get('message_body')
+        message_from = self.request.user.pk if self.request.user.is_authenticated else None
+        tasks.send_feedback_mail.delay(message_body, message_from)
+        return HttpResponseRedirect(reverse_lazy("mainapp:contacts"))
 
 
 class CoursesView(TemplateView):
@@ -110,7 +129,13 @@ class CourseDetailView(TemplateView):
         context_data['course_object'] = get_object_or_404(Courses, pk=self.kwargs.get('pk'))
         context_data['lessons'] = Lesson.objects.filter(course=context_data['course_object'])
         context_data['teachers'] = CourseTeachers.objects.filter(course=context_data['course_object'])
-        context_data['feedback_list'] = CourseFeedback.objects.filter(course=context_data['course_object'])
+        feedback_list_key = f"course_feedback{context_data['course_object'].pk}"
+        cached_feedback_list = cache.get(feedback_list_key)
+        if cached_feedback_list is None:
+            context_data['feedback_list'] = CourseFeedback.objects.filter(course=context_data['course_object'])
+            cache.set(feedback_list_key, context_data['feedback_list'], timeout=600)
+        else:
+            context_data['feedback_list'] = cached_feedback_list
 
         if not self.request.user.is_anonymous:
             if not CourseFeedback.objects.filter(
@@ -140,3 +165,34 @@ class CourseFeedbackFormView(CreateView):
         self.object = form.save()
         rendered_card = render_to_string('mainapp/includes/feedback_card.html', context={'item': self.object})
         return JsonResponse({'card': rendered_card})
+
+
+class LogView(TemplateView):
+    template_name = "mainapp/log_view.html"
+
+    def get_context_data(self, **kwargs):
+        """Переписал как и требовалось в 2b(**).
+        Вывод только последнего кол-ва заданных строк указанных в lines_count"""
+        context = super(LogView, self).get_context_data(**kwargs)
+        log_slice = []
+        lines_count = 10
+        last_line = sum(1 for line in open(settings.LOG_FILE))
+        with open(settings.LOG_FILE, "r") as log_file:
+            for i, line in enumerate(log_file):
+                if i < (last_line - lines_count):
+                    continue
+                log_slice.insert(0, f'{i} {line}')
+            context["log"] = "".join(log_slice)
+        return context
+
+
+class LogDownloadView(UserPassesTestMixin, View):
+
+    def test_func(self):
+        return self.request.user.is_superuser
+
+    def get(self, *args, **kwargs):
+        return FileResponse(open(settings.LOG_FILE, "rb"))
+
+
+
